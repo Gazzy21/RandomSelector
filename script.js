cbfb44c56fefc9dac8451b9cc5d39ec0fef731a8
Gazzy21 <nicholas221@icloud.com>
2024-03-22 14:59:29 -0400

    lots of js but its erroring at trim :(

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const nameArray = []
+const nameArray = [];
 
-function addName () {
-    const nameInput = document.getElementById('nameInput')
-  const name = nameInput.value.trim()
-  nameArray.push(name)
-  displayName()
-  nameInput.value
+function addName() {
+  const nameInput = document.getElementById("nameInput");
+  const name = nameInput.value.trim();
+  nameArray.push(name);
+  displayNames();
+  nameInput.value = "";
 }
 
-document.getElementById('addNameBtn').addEventListener('click', addName)
+function displayNames() {
+  const nameList = document.getElementById("nameList"); //get the UL element
+  nameList.innerHTML = ""; //clears the list
+
+  for (let i = 0; i < nameArray.length; i++) {
+    const name = nameArray[i] //get the current name from the array
+    
+    const li = document.createElement('li')
+    li.className = 'list-group-item'
+
+    const span = document.createElement(span)
+    span.textContent = name
+
+    li.appendChild(span)
+    nameList.appendChild(li)
+    
+  }
+}
+
+document.getElementById("addNameBtn").addEventListener("click", addName);
