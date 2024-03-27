17696bf328275a421337e91b5d059f0062e01938
Gazzy21 <nicholas221@icloud.com>
2024-03-27 14:50:52 -0400

    fixed and finished js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nameArray = [];
 
 function addName() {
   const nameInput = document.getElementById("nameInput");
-  const name = nameInput.value.trim();
+  const name = nameInput.value;
   nameArray.push(name);
   displayNames();
   nameInput.value = "";
@@ -13,18 +13,35 @@ function displayNames() {
   nameList.innerHTML = ""; //clears the list
 
   for (let i = 0; i < nameArray.length; i++) {
-    const name = nameArray[i] //get the current name from the array
-    
-    const li = document.createElement('li')
-    li.className = 'list-group-item'
+    const name = nameArray[i]; //get the current name from the array
 
-    const span = document.createElement(span)
-    span.textContent = name
+    const li = document.createElement("li");
+    li.className = "list-group-item";
 
-    li.appendChild(span)
-    nameList.appendChild(li)
-    
+    const span = document.createElement("span");
+    span.textContent = name;
+
+    li.appendChild(span);
+    nameList.appendChild(li);
   }
 }
 
+function pickRandomName() {
+  const randomNameDiv = document.getElementById("randomName");
+  randomNameDiv.textContent = "";
+
+  const randomNumber = Math.floor(Math.random() * nameArray.length);
+  const randomName = nameArray[randomNumber];
+
+  randomNameDiv.textContent = randomName;
+
+  nameArray.splice(randomNumber, 1);
+
+  displayNames();
+}
+
 document.getElementById("addNameBtn").addEventListener("click", addName);
+
+document
+  .getElementById("pickRandomBtn")
+  .addEventListener("click", pickRandomName);
